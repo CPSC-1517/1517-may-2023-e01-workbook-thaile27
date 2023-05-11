714625709c79f36e8f864ec31bf712ce3499ec98
thaile27 <haile27@hotmail.ca>
2023-05-10 18:35:26 -0600

    Update Program.cs

---
+++ GibHubPracticeSolution/GitHubPractice/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace GitHubPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, the wonderful world of github!");
+
+
+            // Default types
+            Console.WriteLine($"Default type of int data type is: {default(int)}");
+            Console.WriteLine($"Default type of srting data type is: {default(string)}");
+            Console.WriteLine($"Default type of bool data type is: {default(bool)}");
+
+        }
+    }
+}
