76b7ecf0f95014d56914b8b9291ebee1b604c986
thaile27 <haile27@hotmail.ca>
2023-05-24 17:59:19 -0600

    created a public record Residence.cs

---
+++ OOPsSolution/OOPsReview/Residence.cs
@@ -0,0 +1,10 @@
+﻿namespace OOPsReview
+{
+    public record Residence(int Number, string Street, string City, string Province, string PostalCode)
+    {
+        public override string ToString()
+        {
+            return $"{Number},{Street},{City},{Province},{PostalCode}";
+        }
+    }
+}
---
+++ OOPsSolution/Sandbox/Program.cs
@@ -0,0 +1,16 @@
+﻿using OOPsReview;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        Residence myHome = new(123, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+        Console.WriteLine(myHome.ToString());
+
+        // Can i change a value in the record instance? NO!
+        //myHome.Number = 321;
+
+        // To alter the value in the record instance you MUST create new a new instance
+        myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
+        Console.WriteLine(myHome.ToString());
+    }
+}
