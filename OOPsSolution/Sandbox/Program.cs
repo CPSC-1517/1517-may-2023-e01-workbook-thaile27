2aca9f6ecc7ad287f7f0fb93737bbf061772064f
thaile27 <haile27@hotmail.ca>
2023-05-24 18:19:43 -0600

    added example of refactoring

--- OOPsSolution/Sandbox/Program.cs
+++ OOPsSolution/Sandbox/Program.cs
@@ -12,5 +12,49 @@ internal class Program
         // To alter the value in the record instance you MUST create new a new instance
         myHome = new Residence(321, "Maple St.", "Edmonton", "AB", "T6Y7U8");
         Console.WriteLine(myHome.ToString());
+
+        // Example of refactoring
+        // Refactoring is the process of restructuring code, while not changing its original functionality
+        // The goal of refactoring is to improve internal code by making small changes without altering the code external behaviour.
+
+        // Original code
+        bool flag = false;
+        if (myHome.Province.ToLower() == "ab")
+        {
+            flag = true;
+        }
+        if (myHome.Province.ToLower() == "bc")
+        {
+            flag = true;
+        }
+        if (myHome.Province.ToLower() == "sk")
+        {
+            flag = true;
+        }
+        if (myHome.Province.ToLower() == "mn")
+        {
+            flag = true;
+        }
+
+        // Refactor using logical OR operator
+        if (myHome.Province.ToLower() == "ab" || myHome.Province.ToLower() == "bc" || myHome.Province.ToLower() == "sk" || myHome.Province.ToLower() == "mn")
+        {
+            flag = true;
+        }
+
+        // Refactor using a switch statement
+        switch (myHome.Province)
+        {
+            case "ab":
+            case "bc":
+            case "sk":
+            case "mn":
+                flag = true;
+                break;
+            default:
+                flag = false;
+                break;
+        }
+
     }
 }
